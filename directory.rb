ab8692b15fad2feb53d176056decca309708265b
Vivien <vhjtang@gmail.com>
2017-02-28 15:07:48 +0000

    8.3 Added method to print out students with names shorter than 12 characters

--- directory.rb
+++ directory.rb
@@ -33,15 +33,21 @@ def print_footer(students)
 end
 
 def print_names_beginning_with_specific_letter(students, specific_letter)
-
     selected_students = students.select{|student| student[:name][0].downcase == specific_letter.downcase}
     puts
     puts "There are #{selected_students.count} students with names beginning with #{specific_letter}:"
-
     selected_students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
+end
 
+def print_names_shorter_than_12_characters (students)
+  less_than_12 = students.select{|student| student[:name].length < 12}
+  puts
+  puts "There are #{less_than_12.count} students with names that are shorter than 12 characters:"
+  less_than_12.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
 students = input_students
@@ -49,3 +55,4 @@ print_header
 print(students)
 print_footer(students)
 print_names_beginning_with_specific_letter(students, "s")
+print_names_shorter_than_12_characters(students)
