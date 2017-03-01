753822d48c774c70713cfb9dede31c1685570cdb
Vivien <vhjtang@gmail.com>
2017-03-01 19:02:15 +0000

    Ex10 Refactored code

--- Ex9_interactive_menu.rb
+++ Ex9_interactive_menu.rb
@@ -1,47 +1,26 @@
 def interactive_menu
-  students = []
+  @students = [] #an empty array accessible to all methods
   loop do
-  #1. Print the menu and ask user what to do
-  puts "Please choose an option from the menu:"
-  puts
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-  #2. Read the input and save it to variable
-  selection = gets.chomp
-  #3. Do what the user has asked
-  case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant. Please try again."
-    end
+  print_menu
+  process(gets.chomp)
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-#create an empty array
-  students = []
 #get the first name
   name = gets.chomp
 #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
     name = gets.chomp
   end
 #return array of students
-  students
+  @students
 end
 
 def print_header
@@ -49,15 +28,41 @@ def print_header
   puts "--------------"
 end
 
-def print(students)
-  students.each_with_index do |student, index|
+def print_students_list
+  @students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
+def print_menu
+  puts "Please choose an option from the menu:"
+  puts
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant. Please try again."
+    end
+end
 
 interactive_menu
